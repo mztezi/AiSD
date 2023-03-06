edae4c2cbbe4e73e4dde056a0b0616fc77eb8c6f
tezi <michal.rafal.zaborski@gmail.com>
2023-03-06 22:58:50 +0100

    AiSD-7-bubbleSort

---
+++ AiSD-7-bubbleSort.js
@@ -0,0 +1,35 @@
+function swap(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
+
+function bubbleSort(arr) {
+    let swapped;
+
+    do {
+        swapped = false;
+
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] < arr[i]) {
+                swap(arr, i, i + 1);
+                swapped = true;
+            }
+        }
+    } while (swapped)
+}
+
+function bubbleSortOpt(arr) {
+    let n = arr.length;
+
+    do {
+        newn = 0;
+
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i + 1] < arr[i]) {
+                swap(arr, i, i + 1);
+                newn = i;
+            }
+        }
+
+        n = newn;
+    } while (n > 1)
+}
