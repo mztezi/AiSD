35652d5f7346d79efbc21d1b65edb75e12c244b1
tezi <michal.rafal.zaborski@gmail.com>
2023-03-09 23:23:47 +0100

    AiSD-8-insertionSort

---
+++ AiSD-8-insertionSort.js
@@ -0,0 +1,16 @@
+function swap(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
+
+function insertionSort(arr) {
+    for (let i = 1; i <= arr.length; i++) {
+        for (let k = 0; k < i; k++) {
+            if (arr[i - k] < arr[i - k - 1]) {
+                swap(arr, i - k, i - k - 1);
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
