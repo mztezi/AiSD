ce9c773642716d9cf3247f92e7e334b096fb3c73
tezi <michal.rafal.zaborski@gmail.com>
2023-03-12 18:06:30 +0100

    AiSD-2-isPrime update with non-optimal ver

--- isPrime.js
+++ isPrime.js
@@ -3,11 +3,31 @@ function isPrime(n) {
         return true;
     }
 
+    if (n <= 1 || n % 2 === 0) {
+        return false;
+    }
+
+    let SqRoot = Math.sqrt(n);
+
+    for (let i = 3; i <= SqRoot; i += 2) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isPrimeOptimized(n) {
+    if (n === 2 || n === 3) {
+        return true;
+    }
+
     if (n <= 1 || n % 2 === 0 || n % 3 === 0) {
         return false;
     }
 
-    for (let i = 5; i * i < n; n += 6) {
+    for (let i = 5; i * i < n; i += 6) {
         if (n % i === 0 || n % (i + 2) === 0) {
             return false;
         }
