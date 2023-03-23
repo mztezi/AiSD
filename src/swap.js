51eca021f1f44c049ada2a23dbf5e55d27341ae2
tezi <michal.rafal.zaborsk@gmail.com>
2023-03-23 20:11:04 +0100

    AiSD-8-insertionSort upate

---
+++ src/swap.js
@@ -0,0 +1,9 @@
+export function swap(arr, index1, index2) {
+  const tempVal = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = tempVal;
+}
+
+function swapNeat(arr, index1, index2) {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
