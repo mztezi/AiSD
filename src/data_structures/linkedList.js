10b168af377a46954d10785706962533ee2c11bc
tezi <michal.rafal.zaborsk@gmail.com>
2023-05-23 22:47:51 +0200

    AiSD-20 add removeFrom method

--- src/data_structures/linkedList.js
+++ src/data_structures/linkedList.js
@@ -62,6 +62,26 @@ class LinkedList {
         }
     }
 
+    removeFrom(index) {
+        if (index < 0 || index > this.size) {
+            return null;
+        }
+        let removedNode;
+        if (index === 0) {
+            removedNode = this.head;
+            this.head = this.head.next;
+        } else {
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next;
+            this.size--;
+        }
+        return removedNode;
+    }
+
     print() {
         if (this.isEmpty()) {
             console.log("List is empty ");
@@ -92,5 +112,9 @@ console.log('List size ', list.getSize());
 list.print();
 
 list.insert('A', 2);
+console.log('List size ', list.getSize());
+list.print();
+
+list.removeFrom(2);
 console.log('List size ', list.getSize());
 list.print();
